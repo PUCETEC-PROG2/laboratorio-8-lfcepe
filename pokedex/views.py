dd49c3421a0b476e216c09358172cc25a10ab7b1
PUCETEC <felipecepeda2006@hotmail.com>
2024-07-27 09:21:02 -0500

    Añadido iconos, funcion de delete y edit

--- pokedex/views.py
+++ pokedex/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.decorators import login_required
 from .models import Pokemon
@@ -28,8 +28,26 @@ def add_pokemon(request):
     else:
         form = Pokemon_Form()
         
-    return render(request, 'add_pokemon.html', {'form': form})
+    return render(request, 'pokemon_form.html', {'form': form})
 
 class CustomLoginView(LoginView):
     template_name = 'login.html'
     
+@login_required
+def edit_pokemon(request, id):
+    pokemon = get_object_or_404(Pokemon, pk = id)
+    if request.method == 'POST':
+        form = Pokemon_Form(request.POST,request.FILES, instance=pokemon)
+        if form.is_valid():
+            form.save()
+            return redirect('pokedex:index')
+    else:
+        form = Pokemon_Form(instance=pokemon)
+        
+    return render(request, 'pokemon_form.html', {'form': form})
+
+@login_required
+def delete_pokemon(request, id):
+    pokemon = get_object_or_404(Pokemon, pk = id)
+    pokemon.delete()
+    return redirect("pokedex:index")
